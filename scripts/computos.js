9e8bbd7feb09c4492dcea9fe34873e4564b70511
Diego-Vega <ferzux.09@gmail.com>
2026-09-03 21:36:41 -0500

    feat: funcionalidad terminada

--- scripts/computos.js
+++ scripts/computos.js
@@ -3,13 +3,23 @@
   . reporteTotal(info)
   . reporteMasGanan(info)
 */
-function calcularCuotaMensual(prestamo, interes, meses){
-    let aux = Math.pow((1 + interes), meses)
-    let res = aux;
-    return Math.round(res)
-} 
+function calcularCuotaMensual(prestamo, interes, meses) {
+  interes = interes / 100;
+  let aux = Math.pow(1 + interes, meses);
+  let res = (aux * interes) / (aux - 1);
+  res = res * prestamo;
+  return Math.round(res * 100) / 100;
+}
 
+function reporteTotal({ nombre, prestamo, interes, meses, cuota }) {
+  let res = `${nombre} -- $ ${prestamo} - $ ${cuota} - ${meses} meses - ${interes}%\n`;
+  return res;
+}
 
- 
+function reporteMasGanan({ nombre, prestamo, cuota }) {
+  let res = `${nombre} -- $ ${prestamo} - $ ${cuota}\n`;
+  return res;
+}
+
 // listar cada una de las funciones a exportar
-export{calcularCuotaMensual, reporteTotal, reporteMasGanan}
+export { calcularCuotaMensual, reporteMasGanan, reporteTotal };
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 // importa las funciones requeridas desde el modulo escrito en computo.js
-import {calcularCuotaMensual, reporteTotal, reporteMasGanan} from './computos.js';
+import { calcularCuotaMensual, reporteTotal, reporteMasGanan } from './computos.js';
 
 // arreglo para almacenar los datos de cada procesamiento
 let infoCuotas = [];
@@ -8,58 +8,96 @@ let infoCuotas = [];
     obtiene referencias al boton y area de texto donde se va a desplegar la salida.
     almacenarlas en btnFuncionalidad y txtA_Respuesta, respectivamente
 */
-
-
+const btnFuncionalidad = document.getElementById("procesar");
+const txtA_Respuesta = document.getElementById("laRespuesta");
 
 /*
     addEventListener a btnFuncionalidad para que cuando se de un click se
     se ejecute la función procesarFuncionalidad 
 */
+btnFuncionalidad.addEventListener("click", procesarFuncionalidad);
 
 
 /*
-    funcion procesarFuncionalidad
-    Dependiendo de la opcion solicitada desde la pagina web, invoca a:
+    función procesarFuncionalidad
+    Dependiendo de la opción solicitada desde la pagina web, invoca a:
     . procesarEntrada() - para calcular cuota para la entrada dada
     . mostrarReporte1() - despliega el primer reporte solicitado
-    . mostrarReporte1() - despliega el segundo reporte solicitado 
-*/ 
-function procesarFuncionalidad(){ 
+    . mostrarReporte2() - despliega el segundo reporte solicitado 
+*/
+function procesarFuncionalidad() {
     const opcion = document.getElementById("laOpcion").value;
-    switch (opcion){
+    switch (opcion) {
         // completar
+        case "Calcular":
+            procesarEntrada();
+            break;
+        case "ListarTodos":
+            mostrarReporte1();
+            break;
+        case "ListarGanan":
+            mostrarReporte2();
+            break;
+        default:
+            console.log("Opción no reconocida");
     }
 }
 
 /*
-    funcion procesarEntrada
-    Obtiene los inputs de la pagina, calcula la cuota mensual, despliega la informacion
-    obtenida al usuario y crea un objeto con la informacion obtenida y lo almacena en 
+    función procesarEntrada
+    Obtiene los inputs de la pagina, calcula la cuota mensual, despliega la información
+    obtenida al usuario y crea un objeto con la información obtenida y lo almacena en 
     infoCuotas
-*/ 
-function procesarEntrada(){ 
+*/
+function procesarEntrada() {
     // obtener datos de los inputs de la pagina web
+    const nombre = document.getElementById("elNombre").value;
+    const prestamo = document.getElementById("elPrestamo").value;
+    const interes = document.getElementById("elInteres").value;
+    const meses = document.getElementById("losMeses").value;
 
-    // calcular cuota mensual, con la funcion existente en computos.js
+    // calcular cuota mensual, con la función existente en computos.js
+    const cuota = calcularCuotaMensual(prestamo, interes, meses);
 
-    // despliega informacion de la cuota computada
+    // despliega información de la cuota computada
+    txtA_Respuesta.value = `${nombre} debe pagar $ ${cuota} cada mes por el préstamo de $ ${prestamo} a ${meses} meses con el interés de ${interes}%`;
 
-    // crea nuevo objeto con las propiedades nombre, prestamo, meses, interes y cuota
+    // crea nuevo objeto con las propiedades nombre, préstamo, meses, interés y cuota
+    const infoCuota = { nombre, prestamo, interes, meses, cuota };
+
     // y lo almacena al inicio del arreglo infoCuotas
+    infoCuotas.push(infoCuota);
 }
 
 /*
     funcion mostrarReporte1 - despliega el reporte inicial
 */
-function mostrarReporte1(){ 
+function mostrarReporte1() {
+    //Limpiar el area de texto
+    txtA_Respuesta.value = "";
+    // agregar un nuevo elemento con el texto "Reporte 1"
+    txtA_Respuesta.value += "Listado préstamos procesados son:\n\n";
+    // itera sobre cada elemento del arreglo infoCuotas
+    infoCuotas.forEach((info) => {
+        // y despliega el resultado de la función reporteTotal
+        txtA_Respuesta.value += reporteTotal(info);
+    });
 
 }
 
 /*
     funcion mostrarReporte2 - despliega el segundo reporte
 */
-function mostrarReporte2(){ 
-
+function mostrarReporte2() {
+    //Limpiar el area de texto
+    txtA_Respuesta.value = "";
+    // agregar un nuevo elemento con el texto "Reporte 2"
+    txtA_Respuesta.value += "Listado préstamos por más de $ 1'000.000:\n\n";
+    // itera sobre cada elemento del arreglo infoCuotas
+    infoCuotas.forEach((info) => {
+        // y despliega el resultado de la función reporteMasGanan
+        txtA_Respuesta.value += reporteMasGanan(info);
+    });
 }
 
 
